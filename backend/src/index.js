da83ae53bb2884eb1728a5639b1225b0350013fc
Diogo Manhães <dsmanhaes@gmail.com>
2020-09-14 18:43:44 -0300

    feat: add REST functions

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,71 @@
-const express = require ('express');
+const express = require ('express')
+const { v4: uuid } = require ('uuid')
 
-const app = express();
+const app = express()
 
-app.get('/', (request, response) => {
-  return response.json({message: 'Hello World'})
+app.use(express.json())
+
+const projects = []
+
+app.get('/projects', (request, response) => {
+  const { title, owner } = request.query
+
+  const results = (title || owner) ?
+    projects.filter(project => 
+      project.title.includes(title ? title : "") &&
+      project.owner.includes(owner ? owner : "")
+    ) : projects
+
+  return response.json(results)
+})
+
+app.post('/projects', (request, response) => {
+  const { title , owner } = request.body
+  
+  const project = {
+    id: uuid(),
+    title,
+    owner
+  }
+  
+  projects.push(project)
+  
+  return response.json(project)
+})
+
+app.put('/projects/:id', (request, response) => {
+  const { id } = request.params
+  const { title , owner } = request.body
+  const projectIndex = projects.findIndex(project => project.id == id)
+  
+  if (projectIndex < 0){
+    return response.status(400).json({error: 'Project not found'})
+  }
+
+  const project = {
+    id,
+    title,
+    owner
+  }
+
+  projects[projectIndex] = project
+
+  return response.json(project)
+})
+
+app.delete('/projects/:id', (request, response) => {
+  const { id } = request.params
+  const projectIndex = projects.findIndex(project => project.id == id)
+
+  if (projectIndex < 0){
+    return response.status(400).json({error: 'Project not found'})
+  }
+
+  projects.splice(projectIndex, 1)
+  
+  return response.status(204).send()
 })
 
 app.listen(3333, () => {
   console.log('dsLog: 🚀 Back-end launched')
-});
+})
