7795a57fa55af29db6c99bc8ce1299b9dae97d3a
Diogo Manhães <dsmanhaes@gmail.com>
2020-09-16 13:59:57 -0300

    feat: add typescript interface example

--- typescript/dist/index.js
+++ typescript/dist/index.js
@@ -4,8 +4,21 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
+var CreateUser_1 = __importDefault(require("./services/CreateUser"));
 var app = express_1.default();
 app.get('/', function (request, response) {
-    return response.json({ message: 'Hello World' });
+    var user = CreateUser_1.default({
+        email: 'dsmanhaes@gmail.com',
+        password: '123',
+        techs: [
+            'Node.js',
+            'ReactJS',
+            {
+                title: 'React Native',
+                experience: 100
+            }
+        ]
+    });
+    return response.json({ user: user });
 });
 app.listen(3333);
--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,9 +1,23 @@
 import express, { response } from 'express'
+import createUser from './services/CreateUser'
 
 const app = express();
 
 app.get ( '/', ( request, response ) => {
-  return response.json ( { message: 'Hello World' } )
+  const user = createUser ( {
+    email: 'dsmanhaes@gmail.com',
+    password: '123',
+    techs: [
+      'Node.js',
+      'ReactJS', 
+      {
+        title: 'React Native',
+        experience: 100
+      }
+    ]
+  } )
+  
+  return response.json ( { user: user } )
 } )
 
 app.listen(3333)
